ebd8f55931b695a5ce0db79baf30f513f2d1447d
IRCSS <sh.shahrabi@hotmail.com>
2019-11-29 23:59:35 +0100

    added the steam vr+added simple handtracker collider

--- Assets/Scripts/HandTracker.cs
+++ Assets/Scripts/HandTracker.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class HandTracker : MonoBehaviour {
 
+    public  GameObject RightHandRef;
+    public  GameObject LeftHandRef;
+
+    private GameObject rightHandCollider;
+    private GameObject leftHandCollider;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rightHandCollider = this.transform.GetChild(0).gameObject;
+        leftHandCollider  = this.transform.GetChild(1).gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        rightHandCollider.transform.position = RightHandRef.transform.position;
+        leftHandCollider.transform.position  = LeftHandRef.transform.position;
+
 	}
 }
