ef28a8440ca729fe36415859913c74d2430e0c71
Azthewizard <az@bigscreenvr.com>
2019-12-01 17:19:16 +0100

    Interaction assets and code, skybox, scene cleanup
    - added play/pause buttons and interaction logic
    - added skybox pano
    - deleted unnecessary mesh
    - fixed audio sources

---
+++ Assets/Scripts/StartBend.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBend : MonoBehaviour
+{
+    public AudioSource music;
+    public AudioSource binaural;
+    public GameObject itself;
+    public Renderer playRenderer;
+   
+    private bool BUUMactive = false;
+    public Controller startBend;
+  
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider.gameObject.name);
+    
+        if (collider.gameObject.name == "LeftHandCollider")
+        {
+            
+           // OnTriggerEnter.SetActive(true);
+            BUUMactive = true;
+            music.Play();
+            startBend.enabled = true;
+            startBend.StartEffect();
+            Debug.Log("starting bend");
+
+            playRenderer.enabled = false;
+            Debug.Log("BUUMactive is true");
+
+        }
+   
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        binaural.Play();
+    }
+
+    void Update()
+    {
+        if (BUUMactive == true)
+        {
+            binaural.Stop();
+        }
+    }
+    
+}
